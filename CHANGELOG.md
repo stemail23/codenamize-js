5f91c57dc3933b00aa9ee018b6a024ad3928941c
Steven Barlow <stemail23@gmail.com>
2018-10-24 22:11:59 +1100

    Package skeleton framework

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Codenameize Version History
+
+## 0.0.0
+
+- Initial development
---
+++ index.js
@@ -0,0 +1,5 @@
+function codenamize(seed, adjectiveCount, maxItemChars, space, capitalize, hashAlgorithm) {
+	return 'todo: everything!';
+}
+
+module.exports = codenamize;
