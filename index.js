6a4d9dd07e81d870fdd58f5f6cfe10fabc96e7f9
Steven Barlow <stemail23@gmail.com>
2018-10-24 23:27:24 +1100

    - Use `big-integer` package. - Implement calculation of index.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const crypto = require('crypto');
+const bigInt = require('big-integer');
+
 function codenamize(seed, adjectiveCount, maxItemChars, space, capitalize, hashAlgorithm) {
-	return 'todo: everything!';
+	seed = (seed || '').toString();
+	hashAlgorithm = hashAlgorithm || 'md5';
+	const totalWords = 1701900; // TODO: calculate correctly
+	const hash = crypto.createHash(hashAlgorithm);
+	hash.update(seed);
+	const hashDigest = hash.digest('hex');
+	const objHash = bigInt(hashDigest, 16).multiply('36413321723440003717');
+	const index = objHash.mod(totalWords).toJSNumber();
+	return index;
 }
 
 module.exports = codenamize;
