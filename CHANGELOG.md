9771ea98204d10ab78f7f27270b7d2b0b5c55799
Steven Barlow <stemail23@gmail.com>
2018-10-25 22:58:45 +1100

    - Refactor default options to not mutate passed in options object. - JSON.stringify objects (and arrays) when used as seed values.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Codenameize Version History
 
-## 1.0.0
+## Next
+
+- JSON.stringify objects (and arrays) when used as seed values.
+- Remove inadvertently npm published Python Codenamize library subfolder. Oops!
+
+## 1.0.0 (24 Oct 2018)
 
 - Initial release with [Codenamize](https://github.com/jjmontesl/codenamize) equivalence.
--- index.js
+++ index.js
@@ -21,31 +21,59 @@ const adjectiveLengths = findLengths(adjectives);
 nouns.sort((a, b) => (a.length === b.length ? a.localeCompare(b) : a.length - b.length));
 const nounLengths = findLengths(nouns);
 
+function isObject(obj) {
+    return typeof obj === 'object' && !!obj;
+}
+
+function isString(obj) {
+	return Object.prototype.toString.call(obj) === '[object String]';
+}
+
+function isNumber(obj) {
+	return Object.prototype.toString.call(obj) === '[object Number]';
+}
+
+function parseOptions(options) {
+	const response = {};
+
+	response.maxItemChars = options && isNumber(options.maxItemChars) && options.maxItemChars > 0 ? Math.max(3, options.maxItemChars) : 0;
+	if (response.maxItemChars > 9 || !response.maxItemChars) {
+		delete response.maxItemChars;
+	}
+	response.adjectiveCount = options && isNumber(options.adjectiveCount) ? options.adjectiveCount : 0;
+	if (!response.adjectiveCount) {
+		delete response.adjectiveCount;
+	}
+
+	response.seed = (options && options.seed) || '';
+	if (isNumber(response.seed)) {
+		response.seed = response.seed.toString();
+	} else if (isObject(response.seed)) {
+		response.seed = JSON.stringify(response.seed);
+	}
+
+	response.hashAlgorithm = options && isString(options.hashAlgorithm) ? options.hashAlgorithm : 'md5';
+	response.separator = options && isString(options.separator) ? options.separator : '-';
+
+	return response;
+}
+
 function codenameParticles(options) {
-	if (!options || typeof options !== 'object') {
-		options = {};
-	}
-	options.maxItemChars = options.maxItemChars > 0 ? Math.max(3, options.maxItemChars) : 0;
-	if (options.maxItemChars > 9) {
-		options.maxItemChars = 0;
-	}
-	options.adjectiveCount = options.adjectiveCount || 1;
-	options.seed = (options.seed || '').toString();
-	options.hashAlgorithm = options.hashAlgorithm || 'md5';
+	const useOptions = parseOptions(options);
 
 	// Prepare codename word lists and calculate size of codename space
-	const useNouns = options.maxItemChars > 0 ? nouns.slice(0, nounLengths[options.maxItemChars]) : nouns;
-	const useAdjectives = options.maxItemChars > 0 ? adjectives.slice(0, adjectiveLengths[options.maxItemChars]) : adjectives;
+	const useNouns = useOptions.maxItemChars ? nouns.slice(0, nounLengths[useOptions.maxItemChars]) : nouns;
+	const useAdjectives = useOptions.maxItemChars ? adjectives.slice(0, adjectiveLengths[useOptions.maxItemChars]) : adjectives;
 	const particles = [useNouns];
-	for (let i = 0; i < options.adjectiveCount; i++) {
+	for (let i = 0; i < (useOptions.adjectiveCount || 1); i++) {
 		particles.push(useAdjectives);
 	}
 	const totalWords = bigInt(useAdjectives.length)
-		.pow(options.adjectiveCount)
+		.pow(useOptions.adjectiveCount || 1)
 		.multiply(bigInt(useNouns.length));
 
-	const hash = crypto.createHash(options.hashAlgorithm);
-	hash.update(options.seed);
+	const hash = crypto.createHash(useOptions.hashAlgorithm);
+	hash.update(useOptions.seed);
 	const hashDigest = hash.digest('hex');
 	const objHash = bigInt(hashDigest, 16).multiply('36413321723440003717');
 
@@ -62,18 +90,15 @@ function codenameParticles(options) {
 }
 
 function codenamize(options) {
-	if (!options || typeof options !== 'object') {
-		options = {};
-	}
-	options.separator = options.separator || '-';
+	const useOptions = parseOptions(options);
 
-	let particles = codenameParticles(options);
+	let particles = codenameParticles(useOptions);
 
-	if (options.capitalize) {
+	if (useOptions.capitalize) {
 		particles = particles.map(particle => particle.charAt(0).toUpperCase() + particle.substring(1));
 	}
 
-	return particles.join(options.separator);
+	return particles.join(useOptions.separator);
 }
 
 module.exports = codenamize;
